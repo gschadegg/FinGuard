b340e39902ed2b5d5c229d89f8d2f9addfb8c6a4
Gretchen Schadegg <gschadegg@gmail.com>
2025-09-11 22:54:41 -0400

    updates vs settings on save and es lint rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,9 +11,28 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/.pnpm/**',
+      '**/.next/**',
+      '**/out/**',
+      '**/build/**',
+      '**/dist/**',
+    ],
+  },
   ...compat.extends('next/core-web-vitals'),
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    files: ['**/*.{js,jsx}'],
+    rules: {
+      eqeqeq: ['error', 'always'], // error on using == vs ===
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], //ignores unused if beginning with _
+      'jsx-a11y/alt-text': 'warn', // img alt text req
+      'jsx-a11y/anchor-is-valid': 'error', //checks a tags validity
+      'no-console': ['warn', { allow: ['warn', 'error'] }], // checks for console.log
+      curly: 'error',
+      'react/self-closing-comp': ['warn', { component: true, html: true }], //if no children
+    },
   },
   eslintConfigPrettier,
 ]

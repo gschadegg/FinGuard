824d006496361456b7f4da0f1551324175e0531a
Gretchen Schadegg <gschadegg@gmail.com>
2025-09-13 14:15:55 -0400

    adds dark mode toggle

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { ModeToggle } from '@/components/mode-toggle/ModeToggle'
 
 export default function Home() {
   return (
@@ -22,7 +23,8 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
-
+        <div className="bg-background text-foreground">testing ShadCN</div>
+        <ModeToggle />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"

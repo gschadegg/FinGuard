09f9502883a912a79d44f03054810504bfe1adf9
Gretchen Schadegg <gschadegg@gmail.com>
2025-10-02 17:11:42 -0400

    changing capitlization of folder

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -82,7 +82,7 @@ const config = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: ['node_modules', '<rootDir>'],
+  // moduleDirectories: ['node_modules', '<rootDir>'],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -99,11 +99,11 @@ const config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^@/lib/API_URLS$': '<rootDir>/lib/API_URLS/index.js',
-    '^@/lib/API_URLS/index\\.js$': '<rootDir>/lib/API_URLS/index.js',
-  },
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/$1',
+  //   '^@/lib/API_URLS$': '<rootDir>/lib/API_URLS/index.js',
+  //   '^@/lib/API_URLS/index\\.js$': '<rootDir>/lib/API_URLS/index.js',
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
